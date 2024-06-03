63df2e3e9b1db51854c0ff06c5b42e47c2d81179
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-06-03 12:20:57 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,6 +6,7 @@ def feat_eng():
     data = data_vis()
     print("final dataframe----------", data)
     data.replace({'not smoking': 0, 'smoking': 1}, inplace=True)
+    data = data.drop('age_group', axis=1)
     data.to_csv("binary_smoke_prediction.csv", index=False)
 
 
